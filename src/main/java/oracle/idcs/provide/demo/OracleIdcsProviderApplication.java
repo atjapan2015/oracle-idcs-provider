333c9453b9ae4c2f60c16a51dd47f3ba739c6915
atjapan2015 <atjapan2015>
2019-06-20 18:01:06 +0800

    init

---
+++ src/main/java/oracle/idcs/provide/demo/OracleIdcsProviderApplication.java
@@ -0,0 +1,13 @@
+package oracle.idcs.provide.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleIdcsProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleIdcsProviderApplication.class, args);
+	}
+
+}
